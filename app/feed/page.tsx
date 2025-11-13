11feb69916f91c5bf659d02c754b4bd0d9e1d342
membook <dev@local>
2025-11-14 02:57:54 +1100

    UI: add Friends (left) and Chat (right) sidebars to Feed page layout

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,6 +1,8 @@
 import { cookies } from 'next/headers';
 import FeedInfinite from '@/components/FeedInfinite';
 import TopTabs from '@/components/TopTabs';
+import LeftSidebarFriends from '@/components/LeftSidebarFriends';
+import RightSidebarChat from '@/components/RightSidebarChat';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
@@ -10,9 +12,17 @@ export default function FeedPage() {
 	const canPost = !!cookies().get('mem_user_id')?.value;
 	const currentUserId = cookies().get('mem_user_id')?.value || null;
 	return (
-		<div className="space-y-6">
-			<TopTabs currentUserId={currentUserId} />
-			<FeedInfinite canPost={canPost} />
+		<div className="lg:grid lg:grid-cols-12 lg:gap-6">
+			<div className="hidden lg:block lg:col-span-3">
+				<LeftSidebarFriends />
+			</div>
+			<div className="lg:col-span-6 space-y-6">
+				<TopTabs currentUserId={currentUserId} />
+				<FeedInfinite canPost={canPost} />
+			</div>
+			<div className="hidden lg:block lg:col-span-3">
+				<RightSidebarChat />
+			</div>
 		</div>
 	);
 }
